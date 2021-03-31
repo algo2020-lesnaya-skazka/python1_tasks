70f85fd3eb4cc024e7d5831a6e30dd8cd1d1da8e
Sasha331 <72512177+Sasha331@users.noreply.github.com>
2021-03-31 18:42:22 +0300

    add 8183

---
+++ book1_p120-1.py
@@ -0,0 +1,8 @@
+mylist = ['apple', 'milk', 'chesee', 'icecream', 'lemonade', 'tea']
+print(mylist)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+print(mylist[5])
