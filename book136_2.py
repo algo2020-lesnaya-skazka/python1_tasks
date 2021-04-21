445d11f3dcd777e7b5dc98a35a154207486225ad
Sasha331 <72512177+Sasha331@users.noreply.github.com>
2021-04-21 18:59:18 +0300

    ы

---
+++ book136_2.py
@@ -0,0 +1,7 @@
+listA = [1, 2, 3]
+listB = listA
+print('listA =', listA, 'listB =', listB)
+listA[1] = 1000
+print('listA =', listA, 'listB =', listB)
+listB[2] = 75
+print('listA =', listA, 'listB =', listB)
