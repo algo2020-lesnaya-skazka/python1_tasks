00e95e25d9ed5fbed7211255ca8ac4b19778b97b
minecraft556 <71777386+minecraft556@users.noreply.github.com>
2021-04-21 19:04:02 +0300

    Delete book136_2.py

--- book136_2.py
+++
@@ -1,7 +0,0 @@
-listA = [1, 2, 3]
-listB = listA
-print('listA =', listA, 'listB =', listB)
-listA[1] = 1000
-print('listA =', listA, 'listB =', listB)
-listB[2] = 75
-print('listA =', listA, 'listB =', listB)
