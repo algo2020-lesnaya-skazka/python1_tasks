bc85d96b4a890cf798e581218ebd68e6c89c67bb
sofia-Kalinnikova <56312226+sofia-Kalinnikova@users.noreply.github.com>
2021-05-26 18:40:35 +0300

    add previous files

---
+++ python_book_page_115_ex_2.py
@@ -0,0 +1,6 @@
+a = 'FLAMINGO'
+print(a [3])
+print(a[1:7])
+print(a[:3])
+print(a[3:])
+print('it\'s a clody day.')
---
+++ python_book_page_119_ex_2.py
@@ -0,0 +1,4 @@
+g = 'Woof woof'
+print(dog == 'Woof woof')
+print(dog == 'woof woof')
+print(dog == 'Woof woof ')
